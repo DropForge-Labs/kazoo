b9f7c50d3e03b6115418262e78a3cd251cc7f662
Andrew Rosborough <arosborough@acgnw.com>
2014-02-27 18:08:54 -0500

    Refactor flowroute to make numbers_request generic to all api calls

--- core/whistle_number_manager-1.0.0/src/carriers/wnm_flowroute.erl
+++ core/whistle_number_manager-1.0.0/src/carriers/wnm_flowroute.erl
@@ -18,9 +18,19 @@
 
 -define(FR_NUMBER_URL, whapps_config:get_string(?WNM_FR_CONFIG_CAT
                                                    ,<<"numbers_api_url">>
-                                                   ,<<"https://api.flowroute.com/v1/available-tns/tns/">>)).
+                                                   ,<<"https://api.flowroute.com">>)).
+
+-define(FR_AVAILABLE_TNS_PATH, whapps_config:get_string(?WNM_FR_CONFIG_CAT
+                                                   ,<<"numbers_available_tns_path">>
+                                                   ,<<"/v1/available-tns/tns/")).
+
+-define(FR_PURCHASE_TNS_PATH, whapps_config:get_string(?WNM_FR_CONFIG_CAT
+                                                  ,<<"numbers_purchase_tns_path">>
+                                                  ,<<"/v1/tns/")).
 
 -define(FR_DEBUG, whapps_config:get_is_true(?WNM_FR_CONFIG_CAT, <<"debug">>, 'false')).
+
+-type http_verb() :: 'put' | 'post' | 'patch' | 'get' | 'delete'.
 
 %%--------------------------------------------------------------------
 %% @public
@@ -112,31 +122,23 @@ disconnect_number(Number) -> Number.
 %% given verb (purchase, search, provision, ect).
 %% @end
 %%--------------------------------------------------------------------
--spec make_numbers_request/1 :: (proplist()) -> {ok, term()} | {error, term()}.
-make_numbers_request(Props) ->
-    lager:debug("making ~s request to flowroute.com ~s", ["GET", ?FR_NUMBER_URL]),
+-spec make_numbers_request/4 :: (http_verb(), nonempty_string(), wh_json:object(), wh_proplist()) -> {ok, term()} | {error, term()}.
+make_numbers_request(Method, Path, Body, Props) ->
+    lager:debug("making ~s request to flowroute.com ~s", [Method, ?FR_NUMBER_URL]),
     TechPrefix = whapps_config:get_string(?WNM_FR_CONFIG_CAT, <<"tech_prefix">>, <<>>),
     SecretKey = whapps_config:get_string(?WNM_FR_CONFIG_CAT, <<"secret_key">>, <<>>),
     {{Year, Month, Day}, {Hour, Minute, Second}} = calendar:now_to_universal_time(now()),
     Timestamp = lists:flatten(io_lib:format("~B-~2..0B-~2..0BT~2..0B:~2..0B:~2..0B", [Year, Month, Day, Hour, Minute, Second])),
     Query = mochiweb_util:urlencode(Props),
-    Body = "",
     if
-      length(Body) > 0 ->
-        BodyMD5 = wh_util:binary_md5(Body);
-      true ->
-        BodyMD5 = ""
+        length(Query) > 0 ->
+            URL = lists:flatten([?FR_NUMBER_URL, Path, "?", Query]),
+        true ->
+            URL = lists:flatten([?FR_NUMBER_URL, Path])
     end,
-    MessageString = lists:flatten([Timestamp, $\n,
-                                   "GET", $\n,
-                                   wh_util:to_lower_string(BodyMD5), $\n,
-                                   wh_util:to_lower_string(?FR_NUMBER_URL), $\n,
-                                   Query
-                                  ]),
-    Utf8Bin = unicode:characters_to_binary(MessageString, unicode, utf8),
-    <<Signature:20/binary>> = crypto:sha_mac(SecretKey, Utf8Bin),
-    URL = lists:flatten([?FR_NUMBER_URL, "?", Query]),
-    Method = get,
+    URI = lists:flatten([?FR_NUMBER_URL, Path]),
+    Signature = compute_signature(Timestamp, Method, Body, URI, Query),
+
     Headers = [{"Accept", "application/json"}
                ,{"User-Agent", ?WNM_USER_AGENT}
                ,{"X-Timestamp", Timestamp}],
@@ -146,7 +148,7 @@ make_numbers_request(Props) ->
                    ,{connect_timeout, 180000}
                   ],
     ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
-                                      ,io_lib:format("Request:~n~s ~s~n~s~n~s~n~s~n", [get, URL, Body, MessageString, wh_util:to_hex(Signature)])),
+                                      ,io_lib:format("Request:~n~s ~s~n~s~n", [Method, URL, Body])),
     case ibrowse:send_req(URL, Headers, Method, Body, HTTPOptions, 180000) of
         {ok, "401", _, _Response} ->
             ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
@@ -201,6 +203,113 @@ make_numbers_request(Props) ->
             lager:debug("flowroute.com request error: ~p", [E]),
             E
     end.
+make_numbers_request(Method, Number, Body) ->
+    lager:debug("making ~s request to flowroute.com ~s", [Method, ?FR_NUMBER_URL]),
+    TechPrefix = whapps_config:get_string(?WNM_FR_CONFIG_CAT, <<"tech_prefix">>, <<>>),
+    SecretKey = whapps_config:get_string(?WNM_FR_CONFIG_CAT, <<"secret_key">>, <<>>),
+    {{Year, Month, Day}, {Hour, Minute, Second}} = calendar:now_to_universal_time(now()),
+    Timestamp = lists:flatten(io_lib:format("~B-~2..0B-~2..0BT~2..0B:~2..0B:~2..0B", [Year, Month, Day, Hour, Minute, Second])),
+    Query = mochiweb_util:urlencode(Props),
+    if
+        length(Query) > 0 ->
+            URL = lists:flatten([?FR_NUMBER_URL, Path, "?", Query]),
+        true ->
+            URL = lists:flatten([?FR_NUMBER_URL, Path])
+    end,
+    URI = lists:flatten([?FR_NUMBER_URL, Path]),
+    Signature = compute_signature(Timestamp, Method, Body, URI, Query),
+
+    Headers = [{"Accept", "application/json"}
+               ,{"User-Agent", ?WNM_USER_AGENT}
+               ,{"X-Timestamp", Timestamp}],
+    HTTPOptions = [{ssl,[{verify,0}]}
+                   ,{basic_auth, {TechPrefix, wh_util:to_hex(Signature)}}
+                   ,{inactivity_timeout, 180000}
+                   ,{connect_timeout, 180000}
+                  ],
+    ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
+                                      ,io_lib:format("Request:~n~s ~s~n~s~n", [Method, URL, Body])),
+    case ibrowse:send_req(URL, Headers, Method, Body, HTTPOptions, 180000) of
+        {ok, "401", _, _Response} ->
+            ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
+                                              ,io_lib:format("Response:~n401~n~s~n", [_Response])
+                                              ,[append]),
+            lager:debug("flowroute.com request error: 401 (unauthenticated)"),
+            {error, authentication};
+        {ok, "403", _, _Response} ->
+            ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
+                                              ,io_lib:format("Response:~n403~n~s~n", [_Response])
+                                              ,[append]),
+            lager:debug("flowroute.com request error: 403 (unauthorized)"),
+            {error, authorization};
+        {ok, "404", _, _Response} ->
+            ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
+                                              ,io_lib:format("Response:~n404~n~s~n", [_Response])
+                                              ,[append]),
+            lager:debug("flowroute.com request error: 404 (not found)"),
+            {error, not_found};
+        {ok, "500", _, _Response} ->
+            ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
+                                              ,io_lib:format("Response:~n500~n~s~n", [_Response])
+                                              ,[append]),
+            lager:debug("flowroute.com request error: 500 (server error)"),
+            {error, server_error};
+        {ok, "503", _, _Response} ->
+            ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
+                                              ,io_lib:format("Response:~n503~n~s~n", [_Response])
+                                              ,[append]),
+            lager:debug("flowroute.com request error: 503"),
+            {error, server_error};
+        {ok, Code, _, [${,$"|_]=Response} ->
+            ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
+                                              ,io_lib:format("Response:~n~p~n~s~n", [Code, Response])
+                                              ,[append]),
+            lager:debug("received response from flowroute.com"),
+            try
+                JObj = ejson:decode(Response),
+                verify_response(JObj)
+            catch
+                _:R ->
+                    lager:debug("failed to decode json: ~p", [R]),
+                    {error, empty_response}
+            end;
+        {ok, Code, _, _Response} ->
+            ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
+                                              ,io_lib:format("Response:~n~p~n~s~n", [Code, _Response])
+                                              ,[append]),
+            lager:debug("flowroute.com empty response: ~p", [Code]),
+            {error, empty_response};
+        {error, _}=E ->
+            lager:debug("flowroute.com request error: ~p", [E]),
+            E
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Compute a signature for flowroute basic auth
+%% error text
+%% @end
+%%--------------------------------------------------------------------
+-spec compute_signature(nonempty_string(), http_verb(), wh_json:object(), nonempty_string(), string()) -> binary().
+compute_signature(Timestamp, Method, Body, URI, Query)
+    if
+        Method == 'put' || Method == 'post' || Method == 'patch' || length(Body) > 0 ->
+            BodyMD5 = wh_util:binary_md5(Body);
+        true ->
+            BodyMD5 = <<"">>
+    end,
+    MessageString = lists:flatten([Timestamp, $\n,
+                                       Method, $\n,
+                                       wh_util:to_lower_string(BodyMD5), $\n,
+                                       wh_util:to_lower_string(URI), $\n,
+                                       Query
+                                      ]),
+    ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
+                                       ,io_lib:format("Message String: ~n~s~n", [MessageString])
+                                       ,[append]),
+    Utf8Bin = unicode:characters_to_binary(MessageString, unicode, utf8),
+    <<Signature:20/binary>> = crypto:sha_mac(SecretKey, Utf8Bin).
 
 %%--------------------------------------------------------------------
 %% @private
