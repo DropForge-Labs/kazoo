1b66ac8372bb8b039888c74034578859f2a35038
Andrew Rosborough <arosborough@acgnw.com>
2014-03-01 12:22:48 -0500

    Change debug statements

--- core/whistle_number_manager-1.0.0/src/carriers/wnm_flowroute.erl
+++ core/whistle_number_manager-1.0.0/src/carriers/wnm_flowroute.erl
@@ -218,7 +218,7 @@ make_numbers_request(Method, Path, BinBody, Props) ->
                    ,{connect_timeout, 180000}
                   ],
     ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
-                                     ,io_lib:format("Signature: ~s~n~nBody Len: ~s~nHeaders:~n~p~n", [wh_util:to_hex(Signature), length(Body), Headers])
+                                     ,io_lib:format("Signature: ~s~n~nHeaders:~n~p~n", [wh_util:to_hex(Signature), Headers])
                                      ,[append]),
     IsMember = lists:member({"Content-Type", "application/json"}, Headers),
     if
@@ -311,7 +311,7 @@ make_numbers_request(Method, Path, BinBody, Props) ->
             end;
     true ->
         ?FR_DEBUG andalso file:write_file("/tmp/flowroute.com.xml"
-                                         ,io_lib:format("Signature: ~s~n~nBody Len: ~s~nHeaders:~n~p~n", [wh_util:to_hex(Signature), length(Body), Headers])
+                                         ,io_lib:format("Signature: ~s~n~nHeaders:~n~p~n", [wh_util:to_hex(Signature), length(Body), Headers])
                                          ,[append]),
        {error, "No Content-Type Header"}
     end.
